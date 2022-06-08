3e6222549b20e09873b2e39847544cd18df9a7bb
marcos <marcoscabrera021@gmail.com>
2022-06-08 09:34:57 -0300

    cargando tp3

---
+++ TP_[3]/src/tp_3.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : tp_3.c
+ Author      : marcos cabrera - 1 A
+ Version     : 1.0
+ Copyright   : Your copyright notice
+ Description : Trabajo pr�ctico #3
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
